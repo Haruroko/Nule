cb4d4be25f2ef8375e9cf3279620cd85d3f3b9ce
DESKTOP-UN2T0T8\User <digyrh456789@gmail.com>
2025-09-16 15:06:57 +0900

    chore: github pages 배포 과정 중 package.json 수정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Nule',
   experimental: {
     viewTransition: true,
   },
-  /* config options here */
 };
 
 export default nextConfig;
